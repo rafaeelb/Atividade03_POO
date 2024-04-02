c598766103cceda645ff5a3c5f571e05efb24e69
rafaeelb <124620790+rafaeelb@users.noreply.github.com>
2024-04-02 16:06:25 -0300

    Meu primeiro commit

---
+++ atividade03/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Corpo_Humano c1 = new Corpo_Humano();
+        
+        // c1.Calcular_IMC(0, 0);
+    }
+}
---
+++ atividade03/src/Corpo_Humano.java
@@ -0,0 +1,57 @@
+public class Corpo_Humano {
+    private float massa;
+    private float volume;
+    private float densidade;
+    private float altura;
+    private float imc;
+
+    public Corpo_Humano(){
+
+    }
+
+    public Corpo_Humano(float massa, float volume, float altura){
+        this.massa = massa;
+        this.volume = volume;
+        densidade = massa/volume;
+        this.altura = altura;
+        imc = massa/altura * 2;
+    }
+
+    public void setMassa(float massa){
+        this.massa = massa;
+        densidade = massa/volume;
+        imc=massa/altura*2;
+    }
+
+    public void setVolume(float volume){
+        this.volume = volume;
+        densidade = massa/volume;
+    }
+
+
+    public float getMassa(){
+        return massa;
+    }
+
+    public float getVolume(){
+        return volume;
+    }
+
+    public float getDensidade(){
+        return densidade;
+    }
+
+    public void setAltura(float altura){
+        this.altura= altura;
+        imc=massa/altura*2;
+    }
+    public float getAltura(){
+        return altura;
+    }
+
+    public void Calcular_IMC(float massa,  float altura){
+        
+        imc = massa/altura*2;
+    }
+
+}

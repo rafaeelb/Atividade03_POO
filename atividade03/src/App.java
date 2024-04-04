93804aa316a38f7bfad8e70cfb77b17f2c8ae1bc
rafaeelb <rafael2004benicio@gmail.com>
2024-04-04 16:48:05 -0300

    commit change

--- atividade03/src/App.java
+++ atividade03/src/App.java
@@ -1,7 +1,7 @@
 // import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        Corpo_Humano c1 = new Corpo_Humano(70,20.1,1.73);
+        Corpo_Humano c1 = new Corpo_Humano(55,20.1,1.73);
         // JOptionPane.showMessageDialog(null, "A massa é: "+c1.getMassa() + "\nO volume é: " + c1.getVolume());
         System.out.println("A massa é: "+c1.getMassa() + "\nO volume é: " + c1.getVolume());
         System.out.println("A densidade(massa/altura) é : "+c1.getDensidade());

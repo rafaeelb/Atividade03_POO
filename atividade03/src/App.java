dc2281b41e2b65ee6826b02660dbec1bb58b5dc7
rafaeelb <rafael2004benicio@gmail.com>
2024-04-04 20:46:59 -0300

    commit codigo finalizado

--- atividade03/src/App.java
+++ atividade03/src/App.java
@@ -1,18 +1,12 @@
 // import javax.swing.JOptionPane;
-
-import javax.swing.JOptionPane;
-
 public class App {
     public static void main(String[] args) throws Exception {
         Corpo_Humano c1 = new Corpo_Humano(55,20.1,1.73);
-        // System.out.println("A massa é: "+c1.getMassa());
-        // System.out.println("O volume é de: "+c1.getVolume());
-        // System.out.println("A densidade(massa/altura) é : "+c1.getDensidade());
-        // System.out.println("A altura da pessoa é de: " + c1.getAltura());
-        // System.out.println("O IMC da pessoa é: " + c1.getImc());
-        JOptionPane.showMessageDialog(null, "Yasmin meu IMC É " + c1.getImc());
-        JOptionPane.showConfirmDialog(null, "Yasmin vc entendeu? ");
-        JOptionPane.showMessageDialog(null,"OBRIGADO");
+        System.out.println("A massa é: "+c1.getMassa());
+        System.out.println("O volume é de: "+c1.getVolume());
+        System.out.println("A densidade(massa/altura) é : "+c1.getDensidade());
+        System.out.println("A altura da pessoa é de: " + c1.getAltura());
+        System.out.println("O IMC da pessoa é: " + c1.getImc());
 
         // Ao colocar a linha c1.massa = 2 na classe Corpo_Humano, aparece que a varíavel "massa" não foi 
         // declarada e tem um erro de sintaxe. Ao colocar essa mesma linha na classe App, indica que o 
